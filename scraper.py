052c932068860c9fb61bf7cf386fb607d24e93a2
Sebastian Fischer <s164158@student.dtu.dk>
2021-01-06 00:00:54 +0100

    Added scraper.py initial functionality with imgscraper() function
    Now extracts all image IDs in a thread and then downloads (not done yet)

---
+++ scraper.py
@@ -0,0 +1,35 @@
+import re
+import requests
+https = r'https:'
+base = r'//i.4cdn.org/wg/'
+identifier = fr'<a href="{base}'
+main_url = fr'{https}//boards.4chan.org/wg/thread/'
+
+
+def imgscraper(thread_id: str) -> str:
+    request = requests.get(main_url+thread_id)
+    if request.status_code == 200:
+        html_response = request.text
+        regex_pics = r'<a href="//i\.4cdn\.org/wg/(\d{13}\.[a-zA-Z]*)'
+        matches = re.findall(regex_pics, html_response)
+        # print(matches)
+        for pic_id in matches:
+            full_pic_url = fr'{https}{base}{pic_id}'
+            # print(full_pic_url)
+            pic_req = requests.get(full_pic_url)
+
+            # test case right now, only downloads the first image
+            # remove the break and correct new image name for every time
+            # just use the pic id tbh.
+
+            if pic_req.status_code == 200:
+                with open("testpic.jpg",'wb') as picfile:
+                    picfile.write(pic_req.content)
+            break
+    else:
+        raise Exception(f"200 was not returned -> error {request.status_code}")
+
+# <a\s+(?:[^>]*?\s+)?href=(["'])(.*?)\1
+
+# this one works:
+# <a href="\/\/i\.4cdn\.org\/wg\/\d{13}\.[a-zA-Z]*
