d29d9c4728def24d6d456b5b978a77569815e7d0
Sebastian Fischer <s164158@student.dtu.dk>
2021-01-06 13:45:06 +0100

    main.py: updated to spawn pyqt5 gui

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import sys
+from PyQt5.QtWidgets import QApplication
 from scraper import imgscraper
+from main_gui import WgimgscraperApp
+
 
 if __name__ == '__main__':
-    thread_id = "7690437"  # test case
-    exit_code_of_imgscraper: int = imgscraper(thread_id=thread_id)
+    # thread_id = "7690437"  # test case
+    # exit_code_of_imgscraper: int = imgscraper(thread_id=thread_id)
+    gui_app = QApplication(sys.argv)
+    app = WgimgscraperApp()
+    app.show()
+
+    sys.exit(gui_app.exec_())
