808c8f422bd81aa106a8eacddebfd5ccd140fdd5
Sebastian Fischer <s164158@student.dtu.dk>
2021-01-06 17:56:28 +0100

    main.py: removal of legacy code

--- main.py
+++ main.py
@@ -2,10 +2,7 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from main_gui import WgimgscraperApp
 
-
 if __name__ == '__main__':
-    # thread_id = "7690437"  # test case
-    # exit_code_of_imgscraper: int = imgscraper(thread_id=thread_id)
     gui_app = QApplication(sys.argv)
     app = WgimgscraperApp()
     app.show()
