f1cd5dfedd1445276db72086734a0d97e8aa242f
Sebastian Fischer <s164158@student.dtu.dk>
2021-01-06 10:22:49 +0100

    scraper.py: now downloads all images within a thread with a progess bar

--- scraper.py
+++ scraper.py
@@ -1,33 +1,30 @@
+import os
 import re
 import requests
 from constants import *
 from tqdm import tqdm
 
 
-def imgscraper(thread_id: str) -> str:
-    request = requests.get(main_url+thread_id)
+def imgscraper(thread_id: str) -> int:
+    request = requests.get(main_url + thread_id)
     if request.status_code == 200:
+
         html_response = request.text
         regex_pics = r'<a href="//i\.4cdn\.org/wg/(\d{13}\.[a-zA-Z]*)'
         matches = re.findall(regex_pics, html_response)
-        # print(matches)
-        for pic_id in matches:
+
+        for pic_id in tqdm(matches, ascii=True, desc='Downloaded'):  # pic_id example -> 1606735091906.jpg
             full_pic_url = fr'{https}{base}{pic_id}'
-            # print(full_pic_url)
-            pic_req = requests.get(full_pic_url)
-
-            # test case right now, only downloads the first image
-            # remove the break and correct new image name for every time
-            # just use the pic id tbh.
-
-            if pic_req.status_code == 200:
-                with open("testpic.jpg",'wb') as picfile:
-                    picfile.write(pic_req.content)
-            break
+            try:
+                pic_req = requests.get(full_pic_url)
+                if pic_req.status_code == 200:
+                    complete_saving_path = os.path.join(saving_path, pic_id)
+                    with open(f"{complete_saving_path}", 'wb') as picfile:
+                        picfile.write(pic_req.content)
+            except:
+                print(f"Could not download {full_pic_url}")
+                continue
     else:
         raise Exception(f"200 was not returned -> error {request.status_code}")
 
-# <a\s+(?:[^>]*?\s+)?href=(["'])(.*?)\1
-
-# this one works:
-# <a href="\/\/i\.4cdn\.org\/wg\/\d{13}\.[a-zA-Z]*
+    return 0
