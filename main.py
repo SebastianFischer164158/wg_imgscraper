5bfcbf7e5d53d37d0ef663bdb5a2eee24db21386
Sebastian Fischer <s164158@student.dtu.dk>
2021-01-06 10:23:16 +0100

    main.py: updated to contain the exit code of imgscraper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from scraper import imgscraper
 
 if __name__ == '__main__':
-    thread_id = "7690437" # test case
-    x = imgscraper(thread_id=thread_id)
+    thread_id = "7690437"  # test case
+    exit_code_of_imgscraper: int = imgscraper(thread_id=thread_id)
